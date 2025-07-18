0330babb989569244bf83387a6bf7721231e763d
Matthew Morales <mtthwmrls9@gmail.com>
2025-07-18 14:30:34 -0600

    Finish crop tool

--- cropper/crop.py
+++ cropper/crop.py
@@ -8,23 +8,29 @@ from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QPixmap, QPainter, QPen
 from PyQt5.QtCore import Qt, QRect, QPoint, QCoreApplication
 
+class FinalCoords:
+    start = None
+    end = None
+final_coords = FinalCoords()
+
 def get_cli_options ():
     parser = argparse.ArgumentParser(
         prog="Cropper",
     )
 
     parser.add_argument('-d', '--directory')
+    parser.add_argument('-c', '--coords', required=False)
 
     return parser.parse_args(sys.argv[1:])
 
-def do_cropping (startpoint, endpoint):
+def do_cropping ():
     opts = get_cli_options()
     uncropped_dir = Path(opts.directory)
     os.makedirs(os.path.join(str(uncropped_dir.parent), 'CROPPED'+uncropped_dir.name))
     cropped_dir = uncropped_dir.parent.joinpath('CROPPED'+uncropped_dir.name)
     for file in uncropped_dir.iterdir():
         with Image.open(str(file)) as im:
-            im_crop = im.crop((startpoint.x(), startpoint.y(), endpoint.x(), endpoint.y()))
+            im_crop = im.crop((final_coords.start.x(), final_coords.start.y(), final_coords.end.x(), final_coords.end.y()))
             im_crop.save(cropped_dir.joinpath(file.name))
 
 class ImageLabel(QLabel):
@@ -48,7 +54,9 @@ class ImageLabel(QLabel):
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton and self.start_point:
-            do_cropping(self.start_point, self.end_point)
+            final_coords.start = self.start_point
+            final_coords.end = self.end_point
+            do_cropping()
             QCoreApplication.quit()
             
 
@@ -68,9 +76,18 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.image_label)
         self.resize(self.image_label.pixmap.size())
 
+def parse_coords(coords):
+    coordlist = [int(x) for x in coords.split(",")]
+    final_coords.start = QPoint(coordlist[0], coordlist[1])
+    final_coords.end = QPoint(coordlist[2], coordlist[3])
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     opts = get_cli_options()
-    window = MainWindow(str(Path(opts.directory).iterdir().__next__()))  # Replace with your image path
-    window.show()
-    sys.exit(app.exec_())
+    if opts.coords:
+        parse_coords(opts.coords)  
+        do_cropping()  
+    else:
+        window = MainWindow(str(Path(opts.directory).iterdir().__next__()))  # Replace with your image path
+        window.show()
+        sys.exit(app.exec_())
